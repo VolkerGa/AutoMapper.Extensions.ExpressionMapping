be8d1b95cc428334b6287aaff04b14d5f145c150
Volker Gaertner <volker.gaertner@outook.com>
2023-01-08 11:04:37 +0100

    Added yet failing test that does not use Projection

--- tests/Automapper.Extensions.ExpressionMapping.EFCore.UnitTests/ExpressionMappingWithDbSet.cs
+++ tests/Automapper.Extensions.ExpressionMapping.EFCore.UnitTests/ExpressionMappingWithDbSet.cs
@@ -3,6 +3,7 @@ using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 using Shouldly;
+using AutoMapper.QueryableExtensions;
 
 namespace Automapper.Extensions.ExpressionMapping.EFCore.UnitTests
 {
@@ -94,6 +95,50 @@ namespace Automapper.Extensions.ExpressionMapping.EFCore.UnitTests
             /*result.Count.ShouldBe(2);
             result.ShouldAllBe(expOverDTO);*/
         }
+        [Fact]
+        public async void When_Apply_Where_Clause_Over_Queryable_With_Select_FirstOrDefaultAsync()
+        {
+            // Arrange
+            var mapper = CreateMapper();
+
+            // Create and open a connection. This creates the SQLite in-memory database, which will persist until the connection is closed
+            // at the end of the test (see Dispose below).
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            // These options will be used by the context instances in this test suite, including the connection opened above.
+            var contextOptions = new DbContextOptionsBuilder<ModelContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            // Create the schema and seed some data
+            using var context = new ModelContext(contextOptions);
+
+            context.Database.EnsureCreated();
+            context.AddRange(
+                new Model { ABoolean = true },
+                new Model { ABoolean = false },
+                new Model { ABoolean = true },
+                new Model { ABoolean = false });
+            context.SaveChanges();
+
+            var queryable = context.DbModel.AsQueryable<Model>();
+
+            Expression<Func<DTO, bool>> expOverDTO = (dto) => dto.Nested.AnotherBoolean;
+
+            // Act
+            var result = await queryable
+                .UseAsAsyncDataSource(mapper)
+                .For<DTO>()
+                .Where(expOverDTO)
+                .Select(d => new { d.Id })
+                .FirstOrDefaultAsync();
+
+            // Assert
+            result.ShouldNotBeNull();
+            /*result.Count.ShouldBe(2);
+            result.ShouldAllBe(expOverDTO);*/
+        }
         private static IMapper CreateMapper()
         {
             var mapperConfig = new MapperConfiguration(cfg =>
@@ -120,5 +165,6 @@ namespace Automapper.Extensions.ExpressionMapping.EFCore.UnitTests
 
             public DTONested Nested { get; set; }
         }
+
     }
 }
